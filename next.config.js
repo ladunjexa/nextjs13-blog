1cb7fb33d45c6423aaae6ae49bb5ce87f9536ea3
Liron Abutbul <110838700+ladunjexa@users.noreply.github.com>
2025-01-15 00:56:04 +0200

    feat: add support for loading remote images

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.graphassets.com',
+      },
+    ],
+  }
 }
 
 module.exports = nextConfig
